7d1f1045b3e3974caf8524887b495f51f488e9dc
Fabio Traverso <fabio.traverso@gmail.com>
2017-09-07 21:23:55 -0300

    .env

--- src/Base.js
+++ src/Base.js
@@ -1,12 +1,12 @@
 var Rebase = require('re-base');
 var firebase = require('firebase');
 var app = firebase.initializeApp({
-  apiKey: "AIzaSyAA3yyrrYQQzFjKlB1lW1omJCwEmaxLNuk",
-  authDomain: "reactip0.firebaseapp.com",
-  databaseURL: "https://reactip0.firebaseio.com",
-  projectId: "reactip0",
-  storageBucket: "reactip0.appspot.com",
-  messagingSenderId: "746836039406"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 });
 var base = Rebase.createClass(app.database());
 export { base };
